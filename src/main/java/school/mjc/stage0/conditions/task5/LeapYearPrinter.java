9b1c2725b7a3ad5b2436508089a0807e80169f94
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-09-21 03:28:44 +0400

    "commit"

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        System.out.println(year%4==0 && year!=1900? "leap": "not leap");
     }
 }
